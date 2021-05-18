2c3513a45b12b69527ecabe330ffebfabf4da183
Steven Tejeda <steventejeda@gmail.com>
2021-05-18 14:27:06 -0400

    Implemented the A* Algorrithm to find the shortest path. Imported the function into PathFind.js

---
+++ src/aStar.js
@@ -0,0 +1,85 @@
+//To implement the A star search algorithm. For more info please refer to:
+// https://en.wikipedia.org/wiki/A*_search_algorithm
+
+function aStar(start, end) { 
+    //This will contain nodes that the search function has to visit
+    let openSet = [];
+    //This will contain nodes that has already been visited by the search function
+    let closedSet = [];
+    //This will contain the shortest path.
+    let path = [];
+
+    openSet.push(start);
+    //Loop through all of the elements of the openSet
+    while(openSet.length > 0) { 
+        let leastIndex = 0;
+        for(let openIndex = 0; openIndex < openSet.length; openIndex++) { 
+        //Check if 
+            if(openSet[openIndex].f < openSet[leastIndex].f) { 
+                leastIndex = openIndex;
+            }
+        }
+    
+    let current = openSet[leastIndex];
+
+    if(current === end) {
+        let tempVal = current;
+        path.push(tempVal);
+        while(tempVal.previous) { 
+            path.push(tempVal.previous);
+            tempVal = tempVal.previous;
+        }
+        // console.log(path)
+        return path;
+
+        // console.log("Done!, Path found!")
+    }
+    //Remove the current element from the openSet using the filter method. Loop through all of the openSet elements except the current element.
+    //The current element will be removed from the openSet array and pushed through the closedSet array.
+    openSet = openSet.filter(element => element !== current);
+    closedSet.push(current);
+    
+    //
+
+    let neighbors = current.neighbors; 
+    for(let index = 0; index < neighbors.length; index++ ){
+
+        let neighbor = neighbors[index];
+        //Check if closedSet includes neighbor, if it does the search function can move on. 
+        if(!closedSet.includes(neighbor)) { 
+            let tempG = current.g + 1;
+            let newPath = false;
+            //If openSet includes neighbor then the g value will be updated.
+            if(openSet.includes(neighbor)) { 
+                if(tempG < neighbor.g) { 
+                    neighbor.g = tempG;
+                    newPath = true;
+                }
+            } else { 
+                neighbor.g = tempG;
+                newPath = true;
+                openSet.push(neighbor)
+            }
+
+            if(newPath) { 
+                neighbor.h = heruistic(neighbor, end);
+                neighbor.f = neighbor.h + neighbor.g;
+                neighbor.previous = current;
+            }
+        }
+    }
+
+    }
+
+    return {path, error: "No path found!"}
+}
+//Heruistic meaining = self discovery.
+//Utilizing the manhattan distance formula to calculate the distance between two points. 
+//Read more here: https://xlinux.nist.gov/dads/HTML/manhattanDistance.html
+
+function heruistic(a,b) { 
+    let result = Math.abs(a.x - a.y) + Math.abs(b.x - b.y);
+    return result;
+}
+
+export default aStar;
--- src/components/PathFind.js
+++ src/components/PathFind.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import Node from './Node'
+import aStar from "../aStar"
 import '../styles/PathFind.css'
 
 //Variables for columns and rows for the grid
 const cols = 25; 
-const rows = 10;
+const rows = 15;
 
 //Varibles to specify where does rows and cols start and end.
 const start_row = 0;
@@ -14,7 +15,26 @@ const end_col = cols-1;
 
 const PathFind = () => {
     const [Grid, setGrid] = useState([]);
-    
+    const [Path, setPath] = useState([]);
+
+    const createSpot = (grid) => { 
+        //Loop through two dimensional utilizing the inner loop and outer loop
+        for(let outer = 0; outer < rows; outer++) { 
+         for(let inner = 0; inner < cols; inner++) { 
+             grid[outer][inner] = new Spot(outer, inner);
+         }
+     }
+ }
+ //Add Neighbors 
+ const addNeighbors = (grid) => { 
+     //looping through rows and columns to have the ability to add neighbors
+     for(let row = 0; row < rows; row++){ 
+         for(let column = 0; column < cols; column++) {
+             grid[row][column].addNeighbors(grid)
+         }
+
+     }
+ }
     //Function to create a grid and render on the screen.
     const initializeGrid = () => { 
         const grid = new Array(rows);
@@ -23,28 +43,20 @@ const PathFind = () => {
             grid[columns] = new Array(cols);
         }
         createSpot(grid);
-
         setGrid(grid);
+        addNeighbors(grid);
+
+        const start = grid[start_row][start_col];
+        const end = grid[end_row][end_col];
+        let path = aStar(start, end);
+        setPath(path);
 
     };
-
     //To initiliaze grid before anything else is rendered.
     useEffect(() => { 
        initializeGrid();
         }, []);
-
-
-    const createSpot = (grid) => { 
-           //Loop through two dimensional utilizing the inner loop and outer loop
-           for(let inner = 0; inner < rows; inner++) { 
-            for(let outer = 0; outer < cols; outer++) { 
-                grid[inner][outer] = new Spot(inner, outer);
-            }
-        }
-
-
-    }
-    //Spot Constructor
+    //Spot Constructor - could be a seperate file.
     function Spot(row, col) { 
         this.row = row;
         this.column = col;
@@ -52,11 +64,23 @@ const PathFind = () => {
         this.start = this.row === start_row && this.column === start_col
         //If the row and cols equals to the end point, becomes true. Otherwise false.
         this.end = this.row === end_row && this.column === end_col
-        //think of names. Playing a role in the algorithm
+        //Will be used in the heuristic function to calculate the manhattan distance. Playing a role in the algorithm
         this.g = 0;
         this.f = 0;
         this.h = 0;
-
+        //Neighbors will determine the specific spot that the color will have
+        this.neighbors = [];
+        this.previous = undefined;
+        this.addNeighbors = function(grid)
+        {
+            let row = this.row;
+            let column = this.column;
+            // Conditionals to ensure that grid limitations are set. 
+            if (row > 0) this.neighbors.push(grid[row-1][column]);
+            if (row < rows - 1) this.neighbors.push(grid[row+1][column]);
+            if (column > 0) this.neighbors.push(grid[row][column-1]);
+            if (column < cols - 1) this.neighbors.push(grid[row][column+1]);
+        }
     }
  //Grid with Node
 const nodeGrid = (
